51aef29165c9730f705f4864a37ae6d7685be57a
Aung Ye Kaung <aungyekaung.myanmar@gmail.com>
2016-09-17 16:27:47 +0800

    Test

---
+++ ExerciseSessionwithLathaI/SectionB/B1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSessionwithLathaI.SectionB
+{
+    class B1
+    {
+        public static void Main(){ 
+        int n = 3;
+            for (n=0;n<3;n++)
+            {
+                 Console.WriteLine("enter the Number:");
+                 double d= Convert.ToDouble(Console.ReadLine());
+                 Console.WriteLine  ("Input \t\t\t\t  Output");
+                 Console.WriteLine ("{0}\t\t\t\t {1}",d,Math.Sqrt(d));
+
+            }
+
+         }
+        
+      }         
+    }
