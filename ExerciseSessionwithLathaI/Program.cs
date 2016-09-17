1d05de3525d0ec5713d7f11872ed6007073aa49f
Aung Ye Kaung <aungyekaung.myanmar@gmail.com>
2016-09-17 14:34:58 +0800

    17.9.2016

---
+++ ExerciseSessionwithLathaI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSessionwithLathaI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            int j = 4;
+            int sum;
+            if (i < 3)
+            {
+                sum = j - i;
+            }
+            else
+            {
+                sum = j + 8;
+            }
+            showonCommand(sum);
+
+            Console.Write(sum);
+        }
+
+        static void showonCommand(int sum)
+        {
+            Console.WriteLine(sum + "ShowonCommand Method");
+
+        }
+    }
+}
