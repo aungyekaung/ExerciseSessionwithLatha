af215e1864309eb27fb8a6fb14549abc0568274b
E0046776 <1718latha@gmail.com>
2016-10-14 22:39:47 +0800

    section D c6

---
+++ ExerciseSessionwithLathaI/Section D/D2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSessionwithLathaI.Section_D
+{
+    class D2
+    {
+        public static void Main()
+        {
+            int A ; int B ;
+            for(int i=0;i<2;i++)
+            {
+                Console.WriteLine("enter the Numbers:");
+                A = Convert.ToInt32(Console.ReadLine());int A1 = A;
+                B = Convert.ToInt32(Console.ReadLine());int B1 = B;
+                while(A!=B)
+                {
+                    if (B > A)
+                        B = B - A;
+                    else
+                        A = A - B;
+
+                }
+
+                int X = A;  int Y = (A1 * B1) / X;
+                Console.WriteLine("A \t\t B \t\t HCF \t\t LCM");
+                Console.WriteLine("{0} \t\t {1} \t\t {2} \t\t {3}",A1,B1,X,Y);
+
+
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ ExerciseSessionwithLathaI/Section D/D3a.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSessionwithLathaI.Section_D
+{
+    class D3a
+    {
+        public static void Main()
+        {
+            Random r = new Random();
+            int secretnumber = r.Next(1, 11);
+            int guess=0; int counter = 0;
+            while(guess != secretnumber)
+            {
+                Console.WriteLine("Enter A number between 1 and 10");
+                guess = Convert.ToInt32(Console.ReadLine());
+                counter = counter + 1;
+                Console.WriteLine("Please try again");
+            }
+            Console.WriteLine("Congratulation!!");
+            Console.WriteLine("You took {0} attempts", counter);
+        }
+    }
+}
